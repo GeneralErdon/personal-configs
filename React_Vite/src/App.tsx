a261e2d9708243d2a8f4ab148beb22ed9f470dc3
GeneralErdon <leandrofermin@gmail.com>
2024-04-24 09:25:37 -0400

    React Vite config added maybe still needs other pre-configs like the Routing system and Axios, Redux, etc

---
+++ React_Vite/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className=" w-screen h-screen bg-slate-900 text-gray-300 flex items-center justify-center">
+        <span className=" text-[3rem] font-bold">
+          Hello World 😎😎😎😎😎😎😎😎
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
